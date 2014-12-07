182554644363412ba2b98b75de0430e8722eef15
ahsan1808 <ahsanul.karim@c4biz.com>
2014-12-07 05:40:03 -0500

    Google Play services added

--- src/com/smartapps08/storage/GoogleDriveActivity.java
+++ src/com/smartapps08/storage/GoogleDriveActivity.java
@@ -1,15 +1,80 @@
 package com.smartapps08.storage;
 
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
-public class GoogleDriveActivity extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+public class GoogleDriveActivity extends ActionBarActivity implements
+		GoogleApiClient.ConnectionCallbacks,
+		GoogleApiClient.OnConnectionFailedListener {
 	private static final String TAG = StorageDropboxActivity.class.getName();
+	public GoogleApiClient mGoogleApiClient;
+	protected static final int REQUEST_CODE_RESOLUTION = 1;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gdrive);
-		
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Drive.API)
+				.addScope(Drive.SCOPE_FILE).addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).build();
+
 	}
-	
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	@Override
+	public void onConnected(Bundle bundle) {
+		Log.i(TAG, "GoogleApiClient connected");
+	}
+
+	@Override
+	public void onConnectionSuspended(int cause) {
+		Log.i(TAG, "GoogleApiClient connection suspended");
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+		if (!result.hasResolution()) {
+			// show the localized error dialog.
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+					0).show();
+			return;
+		}
+		try {
+			result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+		} catch (SendIntentException e) {
+			Log.e(TAG, "Exception while starting resolution activity", e);
+		}
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_CODE_RESOLUTION && resultCode == RESULT_OK) {
+			mGoogleApiClient.connect();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+		super.onPause();
+	}
+
 }
